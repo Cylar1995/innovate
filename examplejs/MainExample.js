4c6a28925b460da5d81a0a518a5eff5245986bc8
Damian Byron <karatikid1995@gmail.com>
2022-05-16 12:21:42 +0100

    media queries added

---
+++ examplejs/MainExample.js
@@ -0,0 +1,29 @@
+// consdole.log("hello")
+
+// let veriableOne = 10
+
+// if (veriableOne === 10){
+//     console.log(" the veriable wil equil 10")
+// } else if  (veriableOne < 10) {
+
+// } else {
+
+// }
+
+
+
+// function darkmode(){
+//     // trun light mode to dark mode
+// }
+
+// ---------------------------------------
+// java events
+
+text = "one more unto"
+
+function changeText () {
+    Document.getElementByID("shakespeare").innerHTML = text;
+    if (condition==)
+}
+
+
